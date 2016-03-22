26e69bbdfd89988db327e9fa4c31b728fafa7ebb
Dao Quang Phuong <phuongdq@v3s.com.vn>
2016-03-22 18:03:27 +0700

    tam the da

--- installer.js
+++ installer.js
@@ -3,14 +3,6 @@ var globalModule = {
     "babel-preset-react": "^6.5.0",
     "babelify": "^7.2.0",
     "browserify": "^13.0.0",
-    "del": "^2.2.0",
-    "gulp": "^3.9.1",
-    "gulp-concat": "^2.6.0",
-    "gulp-if": "^2.0.0",
-    "gulp-uglify": "^1.5.3",
-    "gulp-uglifycss": "^1.0.6",
-    "vinyl-buffer": "^1.0.0",
-    "vinyl-source-stream": "^1.1.0",
     "watchify": "^3.7.0"
 };
 
